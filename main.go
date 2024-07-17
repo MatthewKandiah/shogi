585cc33dafea4e68f5b01cc08a34cf1bc133bcab
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-07-17 07:28:07 +0100

    rough sketch of minimum app

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 )
 
 func main() {
-	indexComponent := index()
-	signInComponent := signIn()
-
-	http.Handle("/", templ.Handler(indexComponent))
-	http.Handle("/sign-in", templ.Handler(signInComponent))
+	http.Handle("/", templ.Handler(index()))
+	http.Handle("/sign-in", templ.Handler(signIn()))
+	http.Handle("/sign-up", templ.Handler(signUp()))
+	http.Handle("/home", templ.Handler(home()))
+	http.Handle("game", templ.Handler(game()))
 
 	fmt.Println("Listening on :3000")
 	http.ListenAndServe(":3000", nil)
