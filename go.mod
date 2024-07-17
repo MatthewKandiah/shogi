f8bb8d69c1d1f366df7e03b9ee2011eadcc3485f
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-07-17 08:57:25 +0100

    well that's opening a db connection without erroring

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/MatthewKandiah/shogi
 
 go 1.22.5
 
-require (
-	github.com/a-h/templ v0.2.747 // indirect
-	github.com/mattn/go-sqlite3 v1.14.22 // indirect
-)
+require github.com/a-h/templ v0.2.747
+
+require github.com/mattn/go-sqlite3 v1.14.22
--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/a-h/templ"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+
+	db, err := sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	http.Handle("/", templ.Handler(index()))
 	http.Handle("/sign-in", templ.Handler(signIn()))
 	http.Handle("/sign-up", templ.Handler(signUp()))
